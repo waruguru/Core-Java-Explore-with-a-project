5556dbb60294fa9b7dad9d4c254fd750d1f4d31c
Joyce Waruguru Muriithi <jmuriithi4@safaricom.co.ke>
2023-07-28 11:06:46 +0300

    Introduction to Java

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * A class is an entity that can hold fata and behaviour
+ */
+public class Main {
+    /**
+    *public means that the other classes and methods outside this class can have full access to something
+    * static means that main method belongs to this mainClass and can be called by system directly to this class
+     */
+    public static void main(String[] args) {
+        /**
+         * System is a class
+         * out is a reference to an object in System class called Standard output Object
+         * println method of object out
+         * methods re behaviours in which we can tell a program to do something
+         */
+        System.out.println("Hello world!");
+        System.out.println("What do you think?");
+    }
+    /**
+     * main method is entry point of our pplication
+     * loaded first in memory when our application starts
+     * Syntax error when a rule is not obeyed
+     */
+}
